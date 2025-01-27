f1e2ffc19816b35fd65e946b2c5c3b7241407d93
dllll2 <wlsdud8658@gmail.com>
2025-01-27 13:25:13 +0900

    Level 5) 코드 개선 퀴즈 -  JPA의 이해

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -1,5 +1,7 @@
 package org.example.expert.domain.todo.repository;
 
+import java.time.LocalDateTime;
+
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,11 +9,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
-
 public interface TodoRepository extends JpaRepository<Todo, Long>, TodoRepositoryCustom {
 
+    // Level 5) 코드 개선 퀴즈 -  JPA의 이해
     @Query("SELECT t FROM Todo t " +
         "LEFT JOIN FETCH t.user u " +
         "WHERE (:weather IS NULL OR t.weather = :weather) " +
