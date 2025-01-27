d687460ac981b0c6404d1117f5783437772f4df3
dllll2 <wlsdud8658@gmail.com>
2025-01-27 13:09:15 +0900

    Level 3) 코드 개선 퀴즈 - AOP의 이해

--- src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
+++ src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
@@ -19,14 +19,14 @@ public class AdminAccessLoggingAspect {
 
     private final HttpServletRequest request;
 
+    // Level 3) 코드 개선 퀴즈 - AOP의 이해
     @Before("execution(* org.example.expert.domain.user.controller.UserController.getUser(..))")
     public void logAfterChangeUserRole(JoinPoint joinPoint) {
         String userId = String.valueOf(request.getAttribute("userId"));
-        String nickName = String.valueOf(request.getAttribute("nickName"));
         String requestUrl = request.getRequestURI();
         LocalDateTime requestTime = LocalDateTime.now();
 
-        log.info("Admin Access Log - User ID: {}, NickName: {}, Request Time: {}, Request URL: {}, Method: {}",
-                userId, nickName, requestTime, requestUrl, joinPoint.getSignature().getName());
+        log.info("Admin Access Log - User ID: {}, Request Time: {}, Request URL: {}, Method: {}",
+                userId,requestTime, requestUrl, joinPoint.getSignature().getName());
     }
 }
